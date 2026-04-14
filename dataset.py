83878f1fb1973a65111a923e2439f98d046e01c1
akhra92 <akhrorrakhmonov@gmail.com>
2026-04-14 14:33:00 +0900

    Refactor data loading functions to separate training and validation transformations

--- dataset.py
+++ dataset.py
@@ -1,11 +1,23 @@
 import torchvision.transforms as T
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Subset, random_split
 
 import config
 
 
-def get_transform():
+def get_train_transform():
+    return T.Compose([
+        T.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
+        T.RandomHorizontalFlip(),
+        T.RandomVerticalFlip(),
+        T.RandomRotation(15),
+        T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1),
+        T.ToTensor(),
+        T.RandomErasing(p=0.1, scale=(0.02, 0.1)),
+    ])
+
+
+def get_val_transform():
     return T.Compose([
         T.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
         T.ToTensor(),
@@ -13,10 +25,15 @@ def get_transform():
 
 
 def get_dataloaders():
-    transform = get_transform()
-    dataset = ImageFolder(root=config.TRAIN_DATA_PATH, transform=transform)
-    trn_dataset, val_dataset = random_split(dataset, [1 - config.VAL_SPLIT, config.VAL_SPLIT])
+    full_dataset = ImageFolder(root=config.TRAIN_DATA_PATH, transform=None)
+    n_total = len(full_dataset)
+    n_val = int(n_total * config.VAL_SPLIT)
+    n_train = n_total - n_val
+    train_indices, val_indices = random_split(range(n_total), [n_train, n_val])
 
-    train_loader = DataLoader(trn_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=config.BATCH_SIZE, shuffle=False)
+    train_dataset = ImageFolder(root=config.TRAIN_DATA_PATH, transform=get_train_transform())
+    val_dataset = ImageFolder(root=config.TRAIN_DATA_PATH, transform=get_val_transform())
+
+    train_loader = DataLoader(Subset(train_dataset, train_indices), batch_size=config.BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(Subset(val_dataset, val_indices), batch_size=config.BATCH_SIZE, shuffle=False)
     return train_loader, val_loader
